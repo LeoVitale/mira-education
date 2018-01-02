e9ca2d211b4ddd144c60ec32bb3f17fc633fd753
Leonardo Vitale <leonardo_vitale@hotmail.com>
2018-01-02 03:05:42 -0200

    Creating Api Middleware

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const ROOT_URL = 'http://api-test.miraeducacao.com.br';
+
+app.get('/teacher/:id', (req, res) => {
+  const teacherId = req.params.id;
+  axios.get(`${ROOT_URL}/teacher/${teacherId}`)
+    .then(response => {
+      res.send(response.data);
+    });
+});
+
+app.get('/teacher/:id/schoolclasses', (req, res) => {
+  const teacherId = req.params.id;
+  axios.get(`${ROOT_URL}/teacher/${teacherId}/schoolclasses`)
+    .then(response => {
+      res.send(response.data);
+    });
+});
+
+app.get('/students/', (req, res) => {
+  const schoolClassId = req.query.schoolClassId;
+  axios.get(`${ROOT_URL}/students?schoolClassId=${schoolClassId}`)
+    .then(response => {
+      res.send(response.data);
+    });
+})
+
+app.get('/schoolclass/:schollClassId/lessons', (req, res) => {
+  const schollClassId = req.params.schollClassId;
+  axios.get(`${ROOT_URL}/schoolclass/${schollClassId}/lessons`)
+    .then(response => {
+      res.send(response.data);
+    });
+})
+
+app.post('/schoolclass/:schollClassId/lesson', () => {
+  const schollClassId = req.params.schollClassId;
+  axios.post(`${ROOT_URL}/schoolclass/${schollClassId}/lesson`)
+    .then(response => {
+      res.send(response.data);
+    });
+})
+
+app.put('/schoolclass/:schoolClassId/lesson/:lessonId', (req, res) => {
+  const schollClassId = req.params.schollClassId;
+  const lessonId = req.params.lessonId;
+  axios.put(`${ROOT_URL}/schoolclass/${schoolClassId}/lesson/${lessonId}`)
+    .then(response => {
+      res.send(response.data);
+    });
+})
+
+app.delete('/schoolclass/:schoolClassId/lesson/:lessonId', () => {
+  axios.delete(`${ROOT_URL}/schoolclass/${schoolClassId}/lesson/${lessonId}`)
+    .then(response => {
+      res.send(response.data);
+    });
+})
+
+app.listen(3005, () => {
+  console.log('listening on port 3005');
+});
---
+++ src/services/index.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+//const ROOT_URL = 'http://api-test.miraeducacao.com.br';
+const ROOT_URL = 'http://localhost:3005';
+
+//##### GET http://api-test.miraeducacao.com.br/teacher/{teacherId}
+export const getTeacher = (teacherId) => {
+  return axios.get(`${ROOT_URL}/teacher/${teacherId}`);
+}
+
+//##### GET http://api-test.miraeducacao.com.br/teacher/{teacherId}/schoolclasses
+export const getSchoolClasses = (teacherId) => {
+  return axios.get(`${ROOT_URL}/teacher/${teacherId}/schoolclasses`);
+}
+
+//##### GET 	http://api-test.miraeducacao.com.br/students?schoolClassId={schoolClassId}
+export const getClassStudents = (schoolClassId) => {
+  return axios.get(`${ROOT_URL}/students?schoolClassId=${schoolClassId}`);
+}
+
+//##### GET 	http://api-test.miraeducacao.com.br/schoolclass/{schoolClassId}/lessons
+export const getLessons = (schoolClassId) => {
+  return axios.get(`${ROOT_URL}/schoolclass/${schoolClassId}/lessons`);
+}
+
+//##### POST 	http://api-test.miraeducacao.com.br/schoolclass/{schoolClassId}/lesson
+export const newLesson = (schoolClassId) => {
+  return axios.post(`${ROOT_URL}/schoolclass/${schoolClassId}/lesson`);
+}
+
+//##### PUT 	http://api-test.miraeducacao.com.br/schoolclass/{schoolClassId}/lesson/{lessonId}
+export const updateLesson = (schoolClassId, lessonId) => {
+  return axios.put(`${ROOT_URL}/schoolclass/${schoolClassId}/lesson/${lessonId}`);
+}
+
+//##### DELETE	http://api-test.miraeducacao.com.br/schoolclass/{schoolClassId}/lesson/{lessonId}
+export const deleteLessons = (schoolClassId, lessonId) => {
+  return axios.delete(`${ROOT_URL}/schoolclass/${schoolClassId}/lesson/${lessonId}`);
+}
+
+
+// axios.interceptors.request.use(config => {
+//   config.headers = {
+//     'Access-Control-Allow-Origin': '*'
+//   }
+
+//   return config;
+// })
